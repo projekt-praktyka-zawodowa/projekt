18ae893aab005ee0536001868e3163f0956a3aa1
maciej790 <60989274+maciej790@users.noreply.github.com>
2021-04-12 13:58:58 +0200

    projekt

---
+++ projekt/js/script.js
@@ -0,0 +1,26 @@
+function wyskakujaceOkienko() {
+    
+    this.pokaz = function(okienko) {
+        
+        var oknoSzerokosc = window.innerWidth;
+        var oknoWysokosc = window.innerHeight;
+        var animacja = document.getElementById('animacja');
+        var okienko_box_calosc = document.getElementById('okienko_box_calosc');
+        
+        animacja.style.display = "block";
+        animacja.style.height = oknoWysokosc+"px";
+        okienko_box_calosc.style.left = (oknoSzerokosc/2) - (600 * .5)+"px";
+        okienko_box_calosc.style.top = "500px";
+        okienko_box_calosc.style.display = "block";
+        
+        document.getElementById('okienko_box_naglowek').innerHTML = "O zawodzie:";
+        document.getElementById('okienko_box_tresc').innerHTML = okienko;
+        document.getElementById('okienko_box_stopka').innerHTML = '<button onclick="okno.zamknij()"> Wróć do zawodów </button>';
+    }
+	this.zamknij = function() {
+        
+		document.getElementById('okienko_box_calosc').style.display = "none";
+		document.getElementById('animacja').style.display = "none";
+	}
+}
+var okno = new wyskakujaceOkienko();
